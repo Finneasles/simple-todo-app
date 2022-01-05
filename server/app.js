c4f354a694f78a56704faf13a3ab8db6cdac182b
Finneas <85114582+Finneasles@users.noreply.github.com>
2022-01-05 05:28:07 +0000

    App changes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
-var express = require('express');
 var path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var db = require('./db/server.js');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ApiRoute = require('./routes/api');
 
 var app = express();
 
@@ -16,5 +18,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', ApiRoute);
+
+db.mongoose
+  .connect(db.url, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+  })
+  .then(() => {
+     console.log("Connected to the database!");
+  })
+  .catch(err => {
+     console.log("Cannot connect to the database!", err);
+     process.exit();
+  });
 
 module.exports = app;
