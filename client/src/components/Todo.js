7df7ea0c5192aaf7d6f111e0779170ca39991ba5
Finneas <85114582+Finneasles@users.noreply.github.com>
2022-01-06 02:13:26 +0000

    Project changes

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -6,9 +6,11 @@ class Todo extends Component {
   state = {
     todos: []
   }
-  componentDidMont(){
+
+  componentDidUpdate() {
     this.getTodos();
   }
+
   getTodos = () => {
     axios.get('/api/todos')
       .then(res => {
@@ -29,7 +31,6 @@ class Todo extends Component {
  }
 render(){
   let { todos } = this.state;
-  
   return(
     <div>
       <h1>My Todo List</h1>
@@ -39,4 +40,5 @@ render(){
     )
   }
 }
+
 export default Todo;
